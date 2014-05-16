d8bc40be878d932dc09286f2d0f0d7cfc4b7197a
Guillaume Lastecoueres <PX9e@gmx.fr>
2014-05-16 13:59:05 +0200

    general: requirements and PEP8
    * Fixes PEP8.
    
    * Adds requirements.txt.

--- README.md
+++ README.md
@@ -2,3 +2,14 @@ gitbeep
 =======
 
 Ideal for displaying it on a standing monitor/tv and track a repository branch. Everytime a new commit happens, it plays a song.
+
+
+Installation
+============
+
+Run `pip install -r requirements.txt`
+
+Execution
+=========
+
+Run `python go.py`
--- go.py
+++ go.py
@@ -20,7 +20,7 @@ class bcolors:
 def fetch_last_commit(repo):
     r = requests.get(repo)
 
-    if(r.ok):
+    if r.ok:
         repoItem = json.loads(r.text or r.content)
         return repoItem[0]['sha'], repoItem[0]['commit']
 
@@ -32,7 +32,8 @@ def check_committer(name):
 
 
 def print_commit(commit):
-    committer = '%s\n%s just got merged in pu!\033[0m\n\n' % (bcolors.OKGREEN, commit['author']['name'])
+    committer = '%s\n%s just got merged in pu!\033[0m\n\n' % (
+        bcolors.OKGREEN, commit['author']['name'])
     message = '%s' % commit['message']
     print committer, message, '\n'
 
